f4a2051d1cfcb9c5d29a38d08b4187d2e3e8ef5d
walle <daniel.mora.checa@gmail.com>
2017-12-15 02:14:06 +0100

    Reduced amount of examples for simplicity

--- examples/read_data.py
+++ examples/read_data.py
@@ -1,4 +1,4 @@
-from protodata.datasets import BostonSettings, Datasets
+from protodata import datasets
 from protodata.reading_ops import DataReader
 from protodata.data_ops import DataMode
 from protodata.utils import get_data_location
@@ -8,9 +8,14 @@ import tensorflow as tf
 import logging
 logger = logging.getLogger(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
+dataset = datasets.Datasets.MOTOR
+settings_fn = datasets.MotorSettings
+folded = True
 
 tf.app.flags.DEFINE_string('data_location',
-                           get_data_location(Datasets.BOSTON),
+                           get_data_location(dataset, folded=folded),
                            'Path where to build dataset')
 
 tf.app.flags.DEFINE_string('batch_size',
@@ -18,13 +23,13 @@ tf.app.flags.DEFINE_string('batch_size',
                            'Number of instances per batch')
 
 tf.app.flags.DEFINE_integer('memory_factor',
-                            2,
+                            1,
                             'Factor related to the capacity of the queue' +
                             ' (~GB). The higher this amount, the more mixed' +
                             'the data but the slower the processing time')
 
 tf.app.flags.DEFINE_integer('reader_threads',
-                            2,
+                            1,
                             'Number of threads to read the instances.')
 
 FLAGS = tf.app.flags.FLAGS
@@ -35,13 +40,14 @@ if __name__ == '__main__':
     with tf.Session() as sess:
 
         # Boston settings
-        dataset = BostonSettings(dataset_location=FLAGS.data_location)
+        dataset = settings_fn(dataset_location=FLAGS.data_location)
 
         # Read batches from dataset
         reader = DataReader(dataset)
-        features, label = reader.read_batch(
+        features, label = reader.read_folded_batch(
             batch_size=FLAGS.batch_size,
-            data_mode=DataMode.TRAINING,  # Use whatever here, e.g. training
+            data_mode=DataMode.TEST,  # Use whatever here, e.g. training
+            folds=[0, 1, 2],
             memory_factor=FLAGS.memory_factor,
             reader_threads=FLAGS.reader_threads,
             train_mode=False
@@ -60,6 +66,7 @@ if __name__ == '__main__':
         # Print first instance in batch
         idx = 0
         example_idx = {k: v[idx] for k, v in example.items()}
+
         logger.info('Example info: {}'.format(example_idx))
         logger.info('Label: ' + str(l[idx]))
 
